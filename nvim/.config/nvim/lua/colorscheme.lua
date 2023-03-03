a04cb834289657abc0bdabd7ab1d0f67499aa4eb
Andrej-Marsic <andrej_marsic@mckinsey.com>
2023-03-03 21:24:37 +0100

    moar orange

--- nvim/.config/nvim/lua/colorscheme.lua
+++ nvim/.config/nvim/lua/colorscheme.lua
@@ -7,9 +7,13 @@ vim.g["$NVIM_TUI_ENABLE_TRUE_COLOR"] = 1
 -- Example config in Lua
 require("github-theme").setup({
 	theme_style = "dark",
-	function_style = "italic",
+	-- function_style = "italic",
+	hide_inactive_statusline = false,
 	-- sidebars = { "qf", "vista_kind", "terminal", "packer" },
 
 	-- Change the "hint" color to the "orange" color, and make the "error" color bright red
-	-- colors = { hint = "orange", error = "black" },
+	-- ~/.local/share/nvim/site/pack/packer/start/github-nvim-theme/lua/github-theme/palette/dark.lua
+	colors = { syntax = { func = "#ffab70" } },
 })
+
+require("colorizer").setup()
