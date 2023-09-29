1da448d20fab30ab82622d79f4648f1b9e03ba23
Andrej-Marsic <andrej_marsic@mckinsey.com>
2023-09-29 21:35:31 +0200

    pytest is my tester yo

--- nvim/.config/nvim/after/plugin/vimtest.lua
+++ nvim/.config/nvim/after/plugin/vimtest.lua
@@ -1,5 +1,6 @@
 -- vim-test
 -- vim-test options
 vim.g["test#strategy"] = "slimux"
+vim.g["test#python#runner"] = "pytest"
 vim.keymap.set("n", "<leader>tt", ":TestNearest<CR>")
 vim.keymap.set("n", "<leader>tl", ":TestLast<CR>")
