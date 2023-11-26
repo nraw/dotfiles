0b52b304c0deb86f006cf8aa25f869df7d26a6a4
Andrej-Marsic <andrej_marsic@mckinsey.com>
2023-11-26 13:51:38 +0100

    remove default llm prompt

--- nvim/.config/nvim/after/plugin/llm.lua
+++ nvim/.config/nvim/after/plugin/llm.lua
@@ -13,23 +13,4 @@ require("llm").setup({
 	-- hl_group = "Substitute",
 	-- prompts = require('prompt_library'), -- Use this instead of the autoload for faster loading
 	prompts = util.module.autoload("prompt_library"),
-	default_prompt = {
-		provider = openai,
-		builder = function(input)
-			return {
-				temperature = 0.3,
-				max_tokens = 120,
-				messages = {
-					{
-						role = "system",
-						content = "You are helpful assistant.",
-					},
-					{
-						role = "user",
-						content = input,
-					},
-				},
-			}
-		end,
-	},
 })
