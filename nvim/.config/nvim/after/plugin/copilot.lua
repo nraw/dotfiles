182d54ade0c9ee9d01cb3afecf4f9e640a11ebe4
Andrej-Marsic <andrej_marsic@mckinsey.com>
2023-11-26 13:51:17 +0100

    copilot in markdown

--- nvim/.config/nvim/after/plugin/copilot.lua
+++ nvim/.config/nvim/after/plugin/copilot.lua
@@ -1,4 +1,5 @@
 vim.g.copilot_no_tab_map = true
+vim.g.copilot_filetypes = { markdown = true }
 vim.api.nvim_set_keymap("i", "<Right>", 'copilot#Accept("<CR>")', { silent = true, expr = true })
 vim.keymap.set("i", "<Down>", "<Plug>(copilot-next)")
 vim.keymap.set("i", "<Up>", "<Plug>(copilot-previous)")
