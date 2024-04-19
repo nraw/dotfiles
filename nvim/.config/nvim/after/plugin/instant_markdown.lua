9b3cdb9d6d9d2162118b55fa423afc0142bc4539
Andrej-Marsic <andrej_marsic@mckinsey.com>
2024-04-19 17:37:46 +0200

    replacing instant markdown with markdown preview

--- nvim/.config/nvim/after/plugin/instant_markdown.lua
+++
@@ -1,5 +0,0 @@
--- Instant Markdown
--- Instant Markdown options
-vim.g.instant_markdown_autostart = 0
-vim.g.instant_markdown_port = 9898
-vim.keymap.set("n", "<leader>md", ":InstantMarkdownPreview<CR>")
---
+++ nvim/.config/nvim/after/plugin/markdown-preview.lua
@@ -0,0 +1,7 @@
+-- Instant Markdown
+-- Instant Markdown options
+-- vim.g.instant_markdown_autostart = 0
+-- vim.g.instant_markdown_port = 9898
+
+-- Markdown preview
+vim.keymap.set("n", "<leader>md", ":MarkdownPreview<CR>")
