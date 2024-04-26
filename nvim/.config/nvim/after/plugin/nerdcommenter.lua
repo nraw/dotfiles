0fffa661cda17aa3a0cc51ddcce6c29971e10ed4
Andrej-Marsic <andrej_marsic@mckinsey.com>
2024-04-26 19:21:20 +0200

    fix(nerdcommenter): update keybindings and settings

--- nvim/.config/nvim/after/plugin/nerdcommenter.lua
+++ nvim/.config/nvim/after/plugin/nerdcommenter.lua
@@ -2,3 +2,10 @@
 -- NERDCOmmenter options
 vim.g.NERDSpaceDelims = 1 -- Add spaces after comment delimiters by default
 vim.g.NERDDefaultAlign = "left"
+
+-- Disable the default NERDCommenter mappings
+vim.g.NERDCreateDefaultMappings = 0
+
+-- Remap the toggle comment shortcut to <leader>c in normal and visual modes
+vim.keymap.set("n", "<leader>c<space>", "<Plug>NERDCommenterToggle", { noremap = true, silent = true })
+vim.keymap.set("v", "<leader>c<space>", "<Plug>NERDCommenterToggle<CR>", { noremap = true, silent = true })
