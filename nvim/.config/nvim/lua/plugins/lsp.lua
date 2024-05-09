8e603cd81f4c05ce7fb6c9a127cfb13e132cd5a0
Andrej-Marsic <andrej_marsic@mckinsey.com>
2024-05-09 12:46:55 +0200

    one can never have enough formatting

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -52,30 +52,39 @@ return {
 				vim.keymap.set("n", "gd", function()
 					vim.lsp.buf.definition()
 				end, opts("Go to Definition"))
+
 				vim.keymap.set("n", "K", function()
 					vim.lsp.buf.hover()
 				end, opts("Hover"))
+
 				vim.keymap.set("n", "<leader>vws", function()
 					vim.lsp.buf.workspace_symbol()
 				end, opts("Workspace Symbol"))
+
 				vim.keymap.set("n", "<leader>vd", function()
 					vim.diagnostic.open_float()
 				end, opts("Open Float"))
+
 				vim.keymap.set("n", "[d", function()
 					vim.diagnostic.goto_next()
 				end, opts("Diagnostic Next"))
+
 				vim.keymap.set("n", "]d", function()
 					vim.diagnostic.goto_prev()
 				end, opts("Diagnostic Prev"))
+
 				vim.keymap.set("n", "<leader>vca", function()
 					vim.lsp.buf.code_action()
 				end, opts("Code Action"))
+
 				vim.keymap.set("n", "<leader>vrr", function()
 					vim.lsp.buf.references()
 				end, opts("References"))
+
 				vim.keymap.set("n", "<leader>rr", function()
 					vim.lsp.buf.rename()
 				end, opts("Rename"))
+
 				vim.keymap.set("i", "<C-h>", function()
 					vim.lsp.buf.signature_help()
 				end, opts("Signature Help"))
@@ -101,22 +110,21 @@ return {
 			end)
 
 			-- Mason stuff --
-			require('mason').setup({})
-			require('mason-lspconfig').setup()
-			require("mason-lspconfig").setup_handlers {
-					-- The first entry (without a key) will be the default handler
-					-- and will be called for each installed server that doesn't have
-					-- a dedicated handler.
-					function (server_name) -- default handler (optional)
-							require("lspconfig")[server_name].setup {}
-					end,
-					-- Next, you can provide a dedicated handler for specific servers.
-					-- For example, a handler override for the `rust_analyzer`:
-					-- ["ruff"] = function ()
-					--     require("ruff").setup {}
-					-- end
-			}
-
+			require("mason").setup({})
+			require("mason-lspconfig").setup()
+			require("mason-lspconfig").setup_handlers({
+				-- The first entry (without a key) will be the default handler
+				-- and will be called for each installed server that doesn't have
+				-- a dedicated handler.
+				function(server_name) -- default handler (optional)
+					require("lspconfig")[server_name].setup({})
+				end,
+				-- Next, you can provide a dedicated handler for specific servers.
+				-- For example, a handler override for the `rust_analyzer`:
+				-- ["ruff"] = function ()
+				--     require("ruff").setup {}
+				-- end
+			})
 
 			lsp.setup()
 			local luasnip = require("luasnip")
