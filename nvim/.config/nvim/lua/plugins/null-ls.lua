1e240daf71df565e9c0a8cdefafff3cb2bb31b31
Andrej-Marsic <andrej_marsic@mckinsey.com>
2024-05-09 12:47:14 +0200

    I'm still unsure if I set up none-ls correctly

--- nvim/.config/nvim/lua/plugins/null-ls.lua
+++ nvim/.config/nvim/lua/plugins/null-ls.lua
@@ -8,16 +8,22 @@ return {
 		require("mason").setup()
 		require("mason-null-ls").setup({
 			-- Providing an empty handlers will cause all sources to be automatically registered in null-ls
-			handlers = {},
+			handlers = {
+				-- mypy = function(source_name, methods)
+				--   null_ls.builtins.diagnostics.mypy.with({
+				--     extra_args = { "--ignore-missing-imports" },
+				--   })
+				-- end,
+			},
 		})
 
-		-- null_ls.setup({
-		--   sources = {
-		--     null_ls.builtins.formatting.stylua,
-		--     null_ls.builtins.formatting.isort,
-		--     null_ls.builtins.completion.spell,
-		--     -- null_ls.builtins.diagnostics.spellcheck,
-		--   },
-		-- })
+		null_ls.setup({
+			sources = {
+				-- null_ls.builtins.formatting.stylua,
+				-- null_ls.builtins.formatting.isort,
+				-- null_ls.builtins.completion.spell,
+				-- null_ls.builtins.diagnostics.spellcheck,
+			},
+		})
 	end,
 }
