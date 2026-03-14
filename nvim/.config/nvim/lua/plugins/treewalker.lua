bb1ee4e7d597f3c80c40854157211e615cd88a2a
Andrej-Marsic <andrej_marsic@mckinsey.com>
2026-03-14 15:41:17 +0100

    treewalker alt+hjkl and change to alacritty to allow option as alt

---
+++ nvim/.config/nvim/lua/plugins/treewalker.lua
@@ -0,0 +1,14 @@
+return {
+	"aaronik/treewalker.nvim",
+	opts = {},
+	keys = {
+		{ "<A-k>", "<cmd>Treewalker Up<cr>", mode = { "n", "x" }, desc = "Treewalker Up" },
+		{ "<A-j>", "<cmd>Treewalker Down<cr>", mode = { "n", "x" }, desc = "Treewalker Down" },
+		{ "<A-h>", "<cmd>Treewalker Left<cr>", mode = { "n", "x" }, desc = "Treewalker Left" },
+		{ "<A-l>", "<cmd>Treewalker Right<cr>", mode = { "n", "x" }, desc = "Treewalker Right" },
+		{ "<A-K>", "<cmd>Treewalker SwapUp<cr>", desc = "Treewalker SwapUp" },
+		{ "<A-J>", "<cmd>Treewalker SwapDown<cr>", desc = "Treewalker SwapDown" },
+		{ "<A-H>", "<cmd>Treewalker SwapLeft<cr>", desc = "Treewalker SwapLeft" },
+		{ "<A-L>", "<cmd>Treewalker SwapRight<cr>", desc = "Treewalker SwapRight" },
+	},
+}
